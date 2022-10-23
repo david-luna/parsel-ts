dccaef0f7167df74abfb615c96a8e9066d068c06
David Luna <luna.bistuer@gmail.com>
2022-10-23 19:33:19 +0200

    chore: finish nestTokens tests

--- src/nestTokens.spec.ts
+++ src/nestTokens.spec.ts
@@ -5,6 +5,8 @@ describe('nestTokens', () => {
   const tokensRecord: Record<string, Token> = {
     id: { type: 'id', content: '#test', name: 'test', pos: [0, 1] },
     comma: { type: 'comma', content: ',', name: ',', pos: [0, 1] },
+    clazz: { type: 'class', content: '.class', name: 'class', pos: [0, 1] },
+    combinator: { type: 'combinator', content: '>', name: '>', pos: [0, 1] },
   };
 
   it('should return null if tokens is empty', () => {
@@ -19,5 +21,33 @@ describe('nestTokens', () => {
     expect(() => nestTokens([tokensRecord.id, tokensRecord.comma, tokensRecord.comma])).toThrowError(
       /Incorrect comma at 2/,
     );
+  });
+
+  it('should throw error if there is a trailing comma', () => {
+    expect(() => nestTokens([tokensRecord.id, tokensRecord.comma])).toThrowError(/Trailing comma/);
+  });
+
+  it('should return an list object when selectors separated by comma', () => {
+    expect(nestTokens([tokensRecord.id, tokensRecord.comma, tokensRecord.clazz])).toStrictEqual({
+      type: 'list',
+      list: [tokensRecord.id, tokensRecord.clazz],
+    });
+  });
+
+  it('should return NULL when list option is false and no tokens passed', () => {
+    expect(nestTokens([], { list: false })).toStrictEqual(null);
+  });
+
+  it('should return a complex AST when list option is false and there is a combinator', () => {
+    expect(nestTokens([tokensRecord.clazz, tokensRecord.combinator, tokensRecord.id], { list: false })).toStrictEqual({
+      type: 'complex',
+      combinator: tokensRecord.combinator.content,
+      left: tokensRecord.clazz,
+      right: tokensRecord.id,
+    });
+  });
+
+  it('should return a simple token when we only pass this one', () => {
+    expect(nestTokens([tokensRecord.clazz], { list: false })).toStrictEqual(tokensRecord.clazz);
   });
 });
